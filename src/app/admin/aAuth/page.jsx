9162d53de5c119f3c83467ccc8efce67a2ee41d7
sahil <itxsaaho@gmail.com>
2024-08-21 19:37:35 +0530

    advanced commit10

--- src/app/admin/aAuth/page.jsx
+++ src/app/admin/aAuth/page.jsx
@@ -1,19 +1,48 @@
-"use client";
-import React from 'react'
-import { useRouter } from 'next/navigation';
-const IsaAuthenticated = () => {
-    const app =useRouter();
-    const token = localStorage.getItem("token");
-    React.useEffect(() => {
+// "use client";
+// import React from 'react'
+// import { useRouter } from 'next/navigation';
+// const IsaAuthenticated = () => {
+//     const app =useRouter();
+//     const token = localStorage.getItem("token");
+//     React.useEffect(() => {
    
-       if (!token) {
-         app.push("/")
-       }
-     }, [token ]);
-     console.log("token",token)
-  return (
-    <div></div>
-  )
-}
+//        if (!token) {
+//          app.push("/")
+//        }
+//      }, [token ]);
+//      console.log("token",token)
+//   return (
+//     <div></div>
+//   )
+// }
+
+// export default IsaAuthenticated;
+
+"use client";
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+const IsaAuthenticated = () => {
+    const app = useRouter();
+    const [token, setToken] = React.useState(null);
+
+    React.useEffect(() => {
+        // Access localStorage only on the client side
+        if (typeof window !== 'undefined') {
+            const storedToken = localStorage.getItem("token");
+            setToken(storedToken);
+            
+            if (!storedToken) {
+                app.push("/");
+            }
+        }
+    }, [app]); // Empty dependency array ensures this runs once on component mount
+
+    console.log("token", token);
+
+    return (
+        <div></div>
+    );
+};
 
 export default IsaAuthenticated;
